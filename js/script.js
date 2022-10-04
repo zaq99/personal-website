7948de9b9e54fc03a6555c3cf4e81dab7119fa4b
zaq <85076901+zaq99@users.noreply.github.com>
2022-10-05 01:30:22 +0700

    progress javascript 2.0

--- js/script.js
+++ js/script.js
@@ -9,16 +9,36 @@ let btnArt = navigasi[3];
 // ambil parent div
 let parentSection = document.querySelectorAll('.hero > div');
 // ambil semua div
-let secNama = parentSection[0];
-let secAbout = parentSection[1];
-let secPorto = parentSection[2];
-let secArt = parentSection[3];
+let secAsli = parentSection[0];
+let secNama = parentSection[1];
+let secAbout = parentSection[2];
+let secPorto = parentSection[3];
+let secArt = parentSection[4];
 
 
-btnAbout.addEventListener('click', testing);
+btnHome.addEventListener('click',funcHome);
+// btnAbout.addEventListener('click', funcAbout);
+// btnPorto.addEventListener('click',funcPorto)
 
-function testing(event){
-    btnAbout.setAttribute('class','texsPutih')
-    event.preventDefault();
-    console.log(event);
+// function funcHome(event){
+//     btnHome.setAttribute('class','texsPutih')
+//     secNama.setAttribute('class','')
+//     event.preventDefault();
+//     console.log(event.target);
+// }
+
+// function funcAbout(event){
+//     btnAbout.setAttribute('class','texsPutih')
+//     secAbout.setAttribute('class','');
+
+//     secArt.setAttribute('class','hilang')
+//     event.preventDefault();
+//     console.log(event.target);
+// }
+
+function funcHome(event){
+    for(let i of parentSection){
+        console.log(i.attributes['id'])
+    }
+    event.preventDefault()
 }
