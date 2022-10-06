e6d4c51100c852606e7a34e7e3c0db1956af19da
zaq <85076901+zaq99@users.noreply.github.com>
2022-10-06 20:40:02 +0700

    add footer and add responsive.css

--- js/script.js
+++ js/script.js
@@ -32,25 +32,13 @@ btnArt.addEventListener('click',whenOnClick);
 function whenOnClick(event){
     
     //periksa kondisi
-    // jika tombol yang di klick == btnAbout 
     if (event.target == btnAbout){
-        // jalankan function yang bernama aktifkan
-        // dan masukan 2 argumen untuk memunculkan section dan menyalakan link
         aktifkan(secAbout,btnAbout);
-    // jika dan bila jika tombol yang di klick == btnPorto 
     } else if (event.target == btnPorto){
-        // jalankan function yang bernama aktifkan
-        // dan masukan 2 argumen untuk memunculkan section dan menyalakan link
         aktifkan(secPorto, btnPorto);
-    // jika dan bila jika tombol yang di klick == btnArt
     } else if (event.target == btnArt){
-        // jalankan function yang bernama aktifkan
-        // dan masukan 2 argumen untuk memunculkan section dan menyalakan link
         aktifkan(secArt,btnArt);
-    // jika bukan semuanya
     } else {
-        // jalankan function yang bernama aktifkan
-        // dan masukan 2 argumen untuk memunculkan section dan menyalakan link
         aktifkan(secNama,btnHome);
     }
     
@@ -64,15 +52,10 @@ function whenOnClick(event){
 function munculkanPage (pageName){
     // cek semua section yang ada pada parentSection
     for(let i of parentSection){
-        // jika section == data argumen pertama yg diberikan pada function aktifkan
         if(i == pageName){
-            // hapus class pada section tersebut yang bernama 'hilang'
             pageName.classList.remove('hilang')
-            // berikan animasi
             pageName.classList.add('animate__animated', 'animate__fadeInRight' )
-        // jika tidak sama
         } else {
-            // berikan attribute class 'hilang'
             i.setAttribute('class','hilang');
         }
     }
@@ -84,13 +67,9 @@ function munculkanPage (pageName){
 function nyalakanLink(btnName){
     // cek semua link yang ada pada navigasi
     for(let i of navigasi){
-        // jika link == data argumen kedua yg diberikan pada function aktifkan
         if (i == btnName){
-            // berikan attribut class 'teksputih'
             btnName.setAttribute('class','teksPutih')
-        // jika tidak sama
         } else {
-            // hapus class 'teksPutih' pada link tersebut 
             i.classList.remove('teksPutih')
         }
     }
@@ -100,7 +79,6 @@ function nyalakanLink(btnName){
 // function aktifkan untuk menjalankan kedua function sekaligus
 // tangkap 2 argumen yang di berikan sesuai hasil if sebelumya
 function aktifkan(a,b){
-
     // jalankan function dengan parameter yang diterima dari hasil if
     munculkanPage(a)
     nyalakanLink(b)
